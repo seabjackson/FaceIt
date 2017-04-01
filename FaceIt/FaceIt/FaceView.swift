898d24b695f71338256e32670ae0fd46feac485c
Seab <honeyboyseab@gmail.com>
2017-03-31 23:49:27 -0400

    Set up custom view

---
+++ FaceIt/FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Seab on 3/31/17.
+//  Copyright © 2017 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        let skullRadius = min(bounds.size.width, bounds.size.height) / 2
+        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+
+    }
+
+}
+
+
