fe82a36e6a553f1d0d4c79808ec500403fd8e225
Seab <honeyboyseab@gmail.com>
2017-04-15 19:57:14 -0400

    completely set up a new model to monitor expressions and implemented code in our controller to interact with this model and update our View

--- FaceIt/FaceIt/ViewController.swift
+++ FaceIt/FaceIt/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var expression = FacialExpression(eyes: .open, mouth: .grin) {
+        didSet {
+            updateUI()
+        }
     }
-
+    
+    private func updateUI()
+    {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        
+    }
+    
+    private let mouthCurvatures = [
+        FacialExpression.Mouth.grin: 0.5,
+        .frown: -1.0,
+        .smile: 1.0,
+        .neutral: 0.0,
+        .smirk: -0.5
+    ]
 
 }
 
